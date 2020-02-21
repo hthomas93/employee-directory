6f21945190da5256d4965ed4879c1e4f705a1025
Harrison Thomas <hthomas146@utexas.edu>
2020-02-20 20:54:51 -0600

    I have broken up with my old, boring table and am now dating my hotter, newer table

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import React, { useState } from 'react';
 import './App.css';
 import Wrapper from "../src/components/Wrapper"
 import Table from "../src/components/Table"
-import employees from "./employees.json";
-import EmployeeContext from "./utils/EmployeeContext"
 
-const [employeeState, setEmployeeState] = useState({
-  name: "",
-  id: 0,
-  occupation: "",
-  department: ""
-})
 
 function App() {
   return (
-    <EmployeeContext.Provider>
-      <div>
-        <Wrapper>
-          <Table>
-          </Table>
-        </Wrapper>
-      </div >
-    </EmployeeContext.Provider>
+    <div>
+      <Wrapper>
+        <Table>
+        </Table>
+      </Wrapper>
+    </div >
   );
 }
 
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,62 +1,71 @@
-
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import employees from "../../../../employee-directory/";
+import MaterialTable from 'material-table';
 
-
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
-});
-
-function createData(name, id, occupation, department) {
-    return { name, id, occupation, department };
-}
-
-// function employeeArray(employees) {
-//     employees.map(employee => {
-
-//     })
-// }
-
-let rows = [];
-
-export default function SimpleTable() {
-    const classes = useStyles();
+export default function MaterialTableDemo() {
+    const [state, setState] = React.useState({
+        columns: [
+            { title: 'Name', field: 'name' },
+            { title: 'Surname', field: 'surname' },
+            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            {
+                title: 'Birth Place',
+                field: 'birthCity',
+                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+            },
+        ],
+        data: [
+            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+            {
+                name: 'Zerya Betül',
+                surname: 'Baran',
+                birthYear: 2017,
+                birthCity: 34,
+            },
+        ],
+    });
 
     return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="left">Name</TableCell>
-                        <TableCell align="left">Employee ID</TableCell>
-                        <TableCell align="left">Occupation</TableCell>
-                        <TableCell align="left">Department</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <MaterialTable
+            title="Editable Example"
+            columns={state.columns}
+            data={state.data}
+            editable={{
+                onRowAdd: newData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.push(newData);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            if (oldData) {
+                                setState(prevState => {
+                                    const data = [...prevState.data];
+                                    data[data.indexOf(oldData)] = newData;
+                                    return { ...prevState, data };
+                                });
+                            }
+                        }, 600);
+                    }),
+                onRowDelete: oldData =>
+                    new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve();
+                            setState(prevState => {
+                                const data = [...prevState.data];
+                                data.splice(data.indexOf(oldData), 1);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+            }}
+        />
     );
 }
