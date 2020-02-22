48681e788d9f8f879f4d9b6769aa54db45276332
Harrison Thomas <hthomas146@utexas.edu>
2020-02-22 15:52:15 -0600

    Cleaned up code from first attempt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Wrapper from "../src/components/Wrapper"
 import Table from "../src/components/Table"
